0298dae0dd36913eb33368393f66d8c0ea2e562e
isaiasgh <joguherr@espol.edu.ec>
2024-10-28 22:49:02 -0500

    Add HTML animations and update image assets for dark mode

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -13,8 +13,22 @@ toggleButton.addEventListener('click', function() {
     if (body.classList.contains('dark-mode')) {
         localStorage.setItem('dark-mode', 'enabled');
         toggleButton.textContent = '🌞 Modo Claro';
+        switchDarkImage ("img-dev", "backend-developer-darkMode.webp");
+        switchDarkImage ("espol-logo", "espol-logo-darkMode.webp");
     } else {
         localStorage.setItem('dark-mode', 'disabled');
         toggleButton.textContent = '🌙 Modo Oscuro';
+        switchDefaultImage ("img-dev", "backend-developer.webp");
+        switchDefaultImage ("espol-logo", "espol-logo.webp");
     }
 });
+
+function switchDarkImage (className, imgName) {
+    let img = document.getElementsByClassName(className);
+    img[0].setAttribute ("src", "./assets/images/" + imgName);
+}
+
+function switchDefaultImage (className, imgName) {
+    let img = document.getElementsByClassName(className);
+    img[0].setAttribute ("src", "./assets/images/" + imgName);
+}
